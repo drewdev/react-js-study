6a1f3953f18dc0aa53265f2249fd85965ce71a7e
Drew Bisset <andrew.bisset@gmail.com>
2016-03-04 18:45:20 -0500

    Implement nested components

--- app/index.js
+++ app/index.js
@@ -1,16 +1,51 @@
+var USER_DATA = {
+  name: 'Drew Bisset',
+  username: 'drewdev',
+  image: 'https://avatars0.githubusercontent.com/u/1018422?v=3&s=460'
+}
+
 var React = require('react');
 var ReactDOM = require('react-dom');
 
-var HelloWorld = React.createClass({
+var ProfilePic = React.createClass({
   render: function() {
-    console.log(this.props);
+    return <img src={this.props.imageUrl} style={{height: 100, width: 100}} />
+  }
+});
+
+var ProfileLink = React.createClass({
+  render: function() {
     return (
-      <div> Hello {this.props.name} </div>
+      <div>
+        <a href={'https://github.com/' + this.props.username}>
+          {this.props.username}
+        </a>
+      </div>
     )
   }
 });
 
+var ProfileName = React.createClass({
+  render: function() {
+    return (
+      <div>{this.props.name}</div>
+    )
+  }
+});
+
+var Avatar = React.createClass({
+  render: function() {
+    return (
+      <div>
+        <ProfilePic imageUrl={this.props.user.image} />
+        <ProfileName name={this.props.user.name} />
+        <ProfileLink username={this.props.user.username} />
+      </div>
+    )
+  }
+})
+
 ReactDOM.render(
-  <HelloWorld name="Drew" anySortOfData={29} />,
+  <Avatar user={USER_DATA} />,
   document.getElementById('app')
 );
