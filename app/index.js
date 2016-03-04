b3b05bbc98cfbf51647a24cc9f6db4590159e84c
Drew Bisset <andrew.bisset@gmail.com>
2016-03-04 18:45:14 -0500

    Introduce properties to React component

--- app/index.js
+++ app/index.js
@@ -3,13 +3,14 @@ var ReactDOM = require('react-dom');
 
 var HelloWorld = React.createClass({
   render: function() {
+    console.log(this.props);
     return (
-      <div> Hello ReactJS program </div>
+      <div> Hello {this.props.name} </div>
     )
   }
 });
 
 ReactDOM.render(
-  <HelloWorld />,
+  <HelloWorld name="Drew" anySortOfData={29} />,
   document.getElementById('app')
 );
